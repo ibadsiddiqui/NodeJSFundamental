abe6b2a562bce650c0c46dd727013d0e17f51b60
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-06-05 20:29:40 +0500

    flights added and departed

--- 5- Cashing/app.js
+++ 5- Cashing/app.js
@@ -7,3 +7,18 @@ var pdxlax = {
 }
 
 var pl = flight(pdxlax);
+
+pl.triggerDepart();
+
+console.log(pl.getInformation());
+
+var ausdca = {
+    number: 382,
+    origin: 'AUS',
+    destination: "DCA",
+}
+
+var ad = flight(ausdca);
+
+console.log(ad.getInformation());
+console.log(pl.getInformation());
