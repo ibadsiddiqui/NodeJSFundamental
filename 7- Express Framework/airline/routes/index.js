ec86f9b7c8f5e6f76cd5f809496286c4529103b4
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-06-07 11:31:13 +0500

    handling http request with JSON

--- 7- Express Framework/airline/routes/index.js
+++ 7- Express Framework/airline/routes/index.js
@@ -1,9 +1,30 @@
 var express = require('express');
 var router = express.Router();
+var flight = require('../flights')
+
+
+// flight objects
+var flight1 = flight({
+  number: 1,
+  origin: 'LAX',
+  destination: 'DCA',
+  departs: '9AM',
+  arrives: '4PM',
+  
+})
+
+var flight2 = flight({
+  number: 2,
+  origin: 'LAX',
+  destination: 'PDX',
+  departs: '10AM',
+  arrives: '2PM',
+  
+})
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/flights', function(req, res, next) {
+  res.json(flight1.getInformation());
 });
 
 module.exports = router;
