70087cbee2d2dffac2313d060527394a43ae8a13
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-06-10 19:56:27 +0500

    working on RESTful routes

---
+++ 9- RESTful Routes/routes/index.js
@@ -0,0 +1,24 @@
+var express = require('express');
+var router = express.Router();
+var flight = require('../flights')
+var flights = require('../data')
+
+// flight objects
+for (var number in flights){
+  flight[number] = flight(flights[number])
+}
+/* GET home page. */
+
+router.get('/flights/:number', function(req, res, next) {
+
+  var number = req.params('number');
+  
+  if(typeof flights[number] === "undefined"){
+    res.status(404).json({status: 'Error'});
+  } else{
+    res.json(flights[number])
+  }
+
+});
+
+module.exports = router;
