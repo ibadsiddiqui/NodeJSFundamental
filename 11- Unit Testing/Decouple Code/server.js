255c042550c3878cdead5e3bf211efd10fc54655
IBAD SIDDIQUI <ibadsiddiqui01@outlook.com>
2018-06-15 14:42:56 +0500

    flight data added to server

--- 11- Unit Testing/Decouple Code/server.js
+++ 11- Unit Testing/Decouple Code/server.js
@@ -1,5 +1,7 @@
-var http = require('http');
-var app = require('./app');
+var http = require('http'),
+    app = require('./app'),
+    flights = require('../data');
+
 
 http.createServer(app).listen(app.get('port'), ()=>{
     console.log('Express Server listening on port: ' + app.get('port'))
